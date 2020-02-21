f2ef7bcafd7adbb99b408ef29361287aacefc849
Mohamed Amin Elleuch <mohamedamin.elleuch@ads.lan>
2020-02-21 18:45:59 +0100

    add swagger

--- src/main/java/com/example/iiexercise/IiexerciseApplication.java
+++ src/main/java/com/example/iiexercise/IiexerciseApplication.java
@@ -3,7 +3,10 @@ package com.example.iiexercise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class IiexerciseApplication {
 
 	public static void main(String[] args) {
